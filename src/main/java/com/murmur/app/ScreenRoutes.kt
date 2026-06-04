18afaa5cc704f33af120ec38d97a14dda84755b3
andyfriedl <andyfriedl@gmail.com>
2026-06-03 20:23:25 -0500

    Remove unused screen routes

--- src/main/java/com/murmur/app/ScreenRoutes.kt
+++
@@ -1,8 +0,0 @@
-package com.murmur.app
-
-sealed class ScreenRoutes(val route: String) {
-    object Start : ScreenRoutes("start")
-    data class Stream(val streamId: String) : ScreenRoutes("stream/{streamId}") {
-        fun createRoute(id: String) = "stream/$id"
-    }
-}
